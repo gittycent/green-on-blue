0b0f7258dade1b010d0c0c61aea7f50e7132dec0
R. Hossain <rhossain@gmail.com>
2013-03-13 20:56:58 -0400

    Initial commit

---
+++ js/mapswitch.js
@@ -0,0 +1,124 @@
+function switchtokia()
+{
+document.getElementById('heatmap').src = "https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+16dwYzQwv_sG_hBli-M24VGJYyv6UnmD0s-dEGPo&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=5&tmplt=2";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#0F6B99";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+function switchtoincidents()
+{
+document.getElementById('heatmap').src = "https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+16dwYzQwv_sG_hBli-M24VGJYyv6UnmD0s-dEGPo&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=4&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#0F6B99";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+function switchtoRC()
+{
+document.getElementById('heatmap').src =  "https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+16dwYzQwv_sG_hBli-M24VGJYyv6UnmD0s-dEGPo&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=6&tmplt=3";
+document.getElementById('RCbutton').style.background = "#0F6B99";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+function switchto2012District()
+{
+document.getElementById('heatmap').src =
+"https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col5%3E%3E0+from+1N96fvDvaOG96GEDyZGwdVE3-QMTLIOBzuYFkffg&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col5%3E%3E0&y=4&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#0F6B99";
+}
+function switchto2011District()
+{
+document.getElementById('heatmap').src = "https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col5%3E%3E0+from+1N3YI7pCNMRp86Mm0T2VWPfo80k7h8XBa_lD_FNY&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col5%3E%3E0&y=2&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#0F6B99";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+function switchto2010District()
+{
+document.getElementById('heatmap').src =
+"https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col5%3E%3E0+from+1uYf8rA3C13fxH-2fNR3Oq5CSVClch4R2yCFqhck&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col5%3E%3E0&y=2&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#0F6B99";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+
+function switchto2010Province()
+{
+document.getElementById('heatmap').src =
+"https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+1zoPhFyjnwtuCyF0wQ27gmxbnt1n58bcQ-5miXHI&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=2&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#0F6B99";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+
+function switchto2011Province()
+{
+document.getElementById('heatmap').src =
+"https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+18No4GzA5AvKNnGGl4gnXgyXJdzGB8YbS1OuX_ao&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=1&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#0F6B99";
+document.getElementById('2012Pbutton').style.background = "#033E5C";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
+function switchto2012Province()
+{
+document.getElementById('heatmap').src =
+"https://www.google.com/fusiontables/embedviz?viz=MAP&q=select+col8%3E%3E1+from+1FXrgT69YAMF-kJS4CwTN0A1lIb_H-I0xNct5XeY&h=false&lat=33.4465293508735&lng=66.49973672656255&z=6&t=1&l=col8%3E%3E1&y=2&tmplt=1";
+document.getElementById('RCbutton').style.background = "#033E5C";
+document.getElementById('KIAbutton').style.background = "#033E5C";
+document.getElementById('Incidentbutton').style.background = "#033E5C";
+document.getElementById('2010Pbutton').style.background = "#033E5C";
+document.getElementById('2011Pbutton').style.background = "#033E5C";
+document.getElementById('2012Pbutton').style.background = "#0F6B99";
+document.getElementById('2010Dbutton').style.background = "#033E5C";
+document.getElementById('2011Dbutton').style.background = "#033E5C";
+document.getElementById('2012Dbutton').style.background = "#033E5C";
+}
